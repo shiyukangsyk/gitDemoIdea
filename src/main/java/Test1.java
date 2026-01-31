aa6293fa35b6f0c068993cee66d97ba11733dead
shiyukang <2534710542@qq.com>
2026-01-31 16:59:13 +0800

    fourth commit

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
         System.out.println("Hello gitWithIdea!");
         System.out.println("Hello gitWithIdea!2");
         System.out.println("Hello gitWithIdea!3");
+        System.out.println("Hello gitWithIdea!4");
     }
 }
