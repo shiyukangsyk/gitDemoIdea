7698a356b2804f95ebbc6bc43c7ef2fd2dc55a3f
shiyukang <2534710542@qq.com>
2026-01-31 16:58:32 +0800

    third commit

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello gitWithIdea!");
         System.out.println("Hello gitWithIdea!2");
+        System.out.println("Hello gitWithIdea!3");
     }
 }
