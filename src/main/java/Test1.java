68049a64d52e6c3c031c12210fa4a93977f3ff07
shiyukang <2534710542@qq.com>
2026-01-31 17:53:46 +0800

    th commit

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,7 +3,5 @@ public class Test1 {
         System.out.println("Hello gitWithIdea!");
         System.out.println("Hello gitWithIdea!2");
         System.out.println("Hello gitWithIdea!3");
-        System.out.println("Hello gitWithIdea!4");
-        System.out.println("Hello gitWithIdea!5");
     }
 }
