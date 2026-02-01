51f5c089a78116d9ad78070fd2ec0863400ad3d2
shiyukangsyk <2534710542@qq.com>
2026-02-01 21:09:37 +0800

    pull test github

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -10,5 +10,6 @@ public class Test1 {
         System.out.println("hotFix ct");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test github");
     }
 }
